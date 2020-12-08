8c8b8c9b2d65e1f3110369d09fdef19b723cece6
Michelle Wei <weimichelles@gmail.com>
2020-12-08 10:45:39 -0700

    Remove months.txt code from bash code

--- get_month_counts_2020.sh
+++ get_month_counts_2020.sh
@@ -1,13 +1,15 @@
+#Get oa file list from ncbi
 wget https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_file_list.txt
 
+#Get all papers from 2020 and write them to a file
 grep ". 2020" oa_file_list.txt > 2020_papers.txt
 
-touch months.txt
-
+#Count the number of papers from each month in 2020
 for month in $(cat months.txt)
 do grep $month 2020_papers.txt | wc -l >> 2020_month_counts.txt
 done
 
+#Merge the columns of months and the month counts together
 paste months.txt 2020_month_counts.txt > tmp_counts.txt
 mv tmp_counts.txt 2020_month_counts.txt
 
