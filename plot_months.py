b21368c64db904250f9d02fbc31b6c15a6ebd8bd
Michelle Wei <michellewei@Michelles-MacBook-Air.local>
2020-12-17 17:36:23 -0700

    Add python script for plotting month counts

--- plot_months.py
+++ plot_months.py
@@ -2,15 +2,17 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-def plot_month_counts(month_list,counts_list,title):
+def plot_month_counts(counts_list,title,save):
     '''Pass in a list containing months, a list containing
     corresponding number of published paper counts, and a
     string for the graph title'''
-    plt.plot(months,counts)
+    month_list=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    plt.figure()
+    plt.plot(month_list,counts_list)
     plt.xlabel("Month")
     plt.ylabel("Number of Papers Published")
     plt.title(title)
-    plt.show()
+    plt.savefig(save)
 
 def get_list(file_name):
     '''read a single-column file into a list'''
@@ -20,3 +22,9 @@ def get_list(file_name):
         line=int(line)
         file_list.append(line)
     return file_list
+
+
+all_counts_lst=get_list("all_month_counts.txt")
+count_lst_2020=get_list("new_2020_counts.txt")
+plot_month_counts(all_counts_lst,"Monthly paper counts (all time)","all_counts_plot")
+plot_month_counts(count_lst_2020,"Monthly paper counts (2020)","2020_counts_plot")
